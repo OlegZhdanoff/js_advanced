024f6b6eeba16d510bfbf989633ee9b622044c57
OlegZhdanov <stalker08@gmail.com>
2020-12-28 19:54:29 +0500

    t

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 20 },
+    { id: 3, title: 'Keyboard', price: 200 },
+    { id: 4, title: 'Gamepad', price: 50 },
 ];
 //Функция для формирования верстки каждого товара
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
+const renderProduct = (product, img = 'https://placehold.it/200x150') => {
+    return `<div class="col-sm-4 p-4">
+                <div class="panel product-item">
+                    <img src="${img}">
+                    <h4>${product.title}</h4>
+                    <p>${product.price} руб</p>
+                    <button type="button" class="btn btn-primary">В корзину</button>
+                </div>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
 };
 
 renderPage(products);
