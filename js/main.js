ec028defe2a37b2983cc0858098f05b7a78adb32
OlegZhdanov <stalker08@gmail.com>
2021-01-05 01:25:42 +0500

    create buttons for basket elems

--- js/main.js
+++ js/main.js
@@ -174,14 +174,22 @@ class ElemBasket extends ProductItem {
                                         <div class="col-md-4">
                                             <img src="${this.img}" class="card-img" alt="Изображение ${this.title}">
                                         </div>
-                                        <div class="col-md-8">
+                                        <div class="col-md-6">
                                             <div class="card-body">
                                                 <h5 class="card-title">${this.title}</h5>
                                                 <p class="card-text"><small class="text-muted">Количество:
                                                         <b>${this.qty}</b><br>Цена:
-                                                        <b>${this.price} руб.</b></small></p>
-
+                                                        <b>${this.price * this.qty} руб.</b></small></p>
                                             </div>
+                                        </div>
+                                        <div class="col-md-2 d-flex align-items-center"><h6>
+                                        <button type="button" class="btn btn-link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+  <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
+  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+</svg></button><button type="button" class="btn btn-link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-cart-dash" viewBox="0 0 16 16">
+  <path d="M6.5 7a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4z"/>
+  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+</svg></button></h6>
                                         </div>
                                     </div>
                                 </div>`
